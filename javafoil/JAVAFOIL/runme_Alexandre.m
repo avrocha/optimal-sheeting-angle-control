2b998951774f6fa5549f75baed47bf63d8047dae
Alexandre Rocha <avrocha99@gmail.com>
2022-05-25 11:58:20 +0200

    runme_Alexandre: Clean code; Add reference scripts for real experiments data

--- javafoil/JAVAFOIL/runme_Alexandre.m
+++ javafoil/JAVAFOIL/runme_Alexandre.m
@@ -22,17 +22,17 @@ R1     = Rig(26,0); % pivot x,y,
 R1.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord,
 % R1.addFoil(Foil('NACA0009',-5,0,0,Cf)); % foilFile,x,y,dx,chord
 
-R2     = Rig(75,0); % pivot x,y, 
-R2.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord
+% R2     = Rig(75,0); % pivot x,y, 
+% R2.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord
 % R2.addFoil(Foil('NACA0009',-5,0,0,Cf)); % foilFile,x,y,dx,chord,
 
-R3     = Rig(122,0); % pivot x,y,  
-R3.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord
-% R3.addFoil(Foil('NACA0009',-5,0,0,Cf)); % foilFile,x,y,dx,chord
-
-R4     = Rig( 170,0); % pivot x,y,  
-R4.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord
-% R4.addFoil(Foil('NACA0009',-5,0,0,Cf)); % foilFile,x,y,dx,chord
+% R3     = Rig(122,0); % pivot x,y,  
+% R3.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord
+% % R3.addFoil(Foil('NACA0009',-5,0,0,Cf)); % foilFile,x,y,dx,chord
+% 
+% R4     = Rig( 170,0); % pivot x,y,  
+% R4.addFoil(Foil('NACA0018',0,0,6.25,Cw)); % foilFile,x,y,dx,chord
+% % R4.addFoil(Foil('NACA0009',-5,0,0,Cf)); % foilFile,x,y,dx,chord
 
 ship.addRig(R1);  
 % ship.addRig(R2);  
@@ -48,7 +48,7 @@ ship.yaw = deg2rad(0);
 ship.yaw = deg2rad(60);
 [~,~,~] = calc_objective([deg2rad(-180), 0, 0, 0]);
 
-%% cL, cD curve - 2D
+%% cL, cD curve - 1D
 ship.yaw = deg2rad(0);
 AoA = deg2rad(linspace(-20, 45, 66)); % sheeting angle = AoA
 cL = zeros(length(AoA), 1); 
@@ -70,7 +70,7 @@ plot(rad2deg(AoA), cD, 'r--', 'Linewidth', 1)
 xlabel('AoA (º)');
 legend('cL', 'cD');
 
-%% cT curves - 2D
+%% cT curves - 1D
 sheeting_angle = linspace(deg2rad(-180), deg2rad(180));
 yaw = deg2rad([150, 120, 90, 60]);
 cT = zeros(length(yaw), length(sheeting_angle)); % Thrust
@@ -101,16 +101,18 @@ ylabel('cT');
 legendCell = cellstr(num2str(rad2deg(yaw'), 'AWA=%.0dº'));
 legend(legendCell);
 
-%% cT surface - 1 sail
-sheeting_angle = linspace(deg2rad(-180), deg2rad(180));
-yaw = linspace(deg2rad(0), deg2rad(180));
+%% cT surface 1D - Time-variant AWA
+sheeting_angle = linspace(deg2rad(-180), deg2rad(180), 180);
+yaw = linspace(deg2rad(-180), deg2rad(180), 180);
 cT = zeros(length(yaw), length(sheeting_angle));
 
 tic
 for i = 1: length(yaw)
     ship.yaw = yaw(i);
     for j = 1:length(sheeting_angle)
+        tic
         [~,~,cT(i, j),~] = calc_objective(sheeting_angle(j));
+        t = toc;
     end
 end
 toc
@@ -132,7 +134,7 @@ title('cT(AWA, $\delta_s$)', 'Interpreter', 'Latex')
 xlabel('AWA [deg]', 'Interpreter', 'Latex');
 ylabel('sheeting angle $\delta_s$ [deg]', 'Interpreter', 'Latex');
 
-%% cT - Print surface
+%% cT surface 1D - Print surface
 load('cT_SA_AWA.mat')
 
 figure(10); clf(10); hold on;
@@ -150,7 +152,7 @@ title('cT(AWA, $\delta_s$)', 'Interpreter', 'Latex')
 xlabel('AWA [deg]', 'Interpreter', 'Latex');
 ylabel('sheeting angle $\delta_s$ [deg]', 'Interpreter', 'Latex');
 
-%% cT surface - 2 sail (fixed AWA)
+%% cT surface 2D - Fixed AWA
 ship.yaw = deg2rad(45);
 ship.addRig(R2)
 scale = calc_scale();
@@ -185,7 +187,7 @@ title('cT($\delta_s^1, \delta_s^2$) | AWA = 45', 'Interpreter', 'Latex')
 xlabel('sheeting angle 1 $\delta_s^1$ [deg]', 'Interpreter', 'Latex');
 ylabel('sheeting angle 2 $\delta_s^2$ [deg]', 'Interpreter', 'Latex');
 
-%% cT 2D - Print surface
+%% cT surface 2D - Print surface
 load('cT1_cT2_SA.mat')
 
 cT = data.cT;
@@ -200,18 +202,114 @@ title('cT($\delta_s^1, \delta_s^2$) | AWA = 45', 'Interpreter', 'Latex')
 xlabel('sheeting angle 1 $\delta_s^1$ [deg]', 'Interpreter', 'Latex');
 ylabel('sheeting angle 2 $\delta_s^2$ [deg]', 'Interpreter', 'Latex');
 
-%% Hessian plot
-load('cT_SA_AWA.mat')
+%% %% cT data - 2 sail, time-variant AWA
+sheeting_angle_1 = linspace(deg2rad(-180), deg2rad(180), 3);
+sheeting_angle_2 = linspace(deg2rad(-180), deg2rad(180), 3);
+AWA = linspace(deg2rad(-180), deg2rad(180), 2);
+cT = zeros(length(AWA), length(sheeting_angle_1), length(sheeting_angle_2));
 
+tic
+for k = 1:length(AWA)
+    ship.yaw = AWA(k);
+    for i = 1: length(sheeting_angle_1)
+        for j = 1:length(sheeting_angle_2)
+            [~,~,cT(k, i, j),~] = calc_objective([sheeting_angle_1(i), sheeting_angle_2(j)]);
+        end
+    end
+end
+toc
+
+% Uncomment lines below to save struct
+data.cT = cT;
+data.awa = AWA;
+data.sheeting_angle_1 = sheeting_angle_1;
+data.sheeting_angle_2 = sheeting_angle_2;
+save('cT_2D_SA_AWA.mat', 'data')
+
+%% Get slope reference
+load('cT_SA_AWA.mat')
 AWA = 45;
 [~, i] = min(abs(data.x_grid(:, 1) - AWA));
+cT_data = data.cT(i,  data.y_grid(1,:) < 20); % For AWA > 0 
+[~, i] = max(cT_data);
+data.y_grid(1, i);
+slope = (cT_data(i-1) - cT_data(i)) / (data.y_grid(1, i-1) - data.y_grid(1, i))
 
-cT_1D = data.cT(26, :);
+%% References for 7M data
+dir = 'plots\7m_data_';
 
-figure;
-subplot(3,1,1)
-plot(linspace(-180, 180, length(cT_1D)), cT_1D, 'Linewidth', 2)
-subplot(3,1,2)
-plot(linspace(-180, 180, length(cT_1D)), gradient(cT_1D), 'Linewidth', 2)
-subplot(3,1,3)
-plot(linspace(-180, 180, length(cT_1D)), gradient(gradient(cT_1D)), 'Linewidth', 2)
+% Tacking procedure
+sheeting_angle = linspace(deg2rad(-70), deg2rad(70), 70);
+yaw = linspace(deg2rad(-80), deg2rad(80), 80);
+cT = zeros(length(yaw), length(sheeting_angle));
+
+tic
+for i = 1: length(yaw)
+    ship.yaw = yaw(i);
+    for j = 1:length(sheeting_angle)
+        [~,~,cT(i, j),~] = calc_objective(sheeting_angle(j));
+    end
+end
+toc
+
+figure(10); clf(10); hold on;
+[X,Y] = meshgrid(rad2deg(yaw), rad2deg(sheeting_angle));
+
+% Uncomment lines below to save struct
+data.cT = cT;
+data.x_grid = X';
+data.y_grid = Y';
+save(strcat(dir,'tacking\cT_SA_AWA.mat'), 'data');
+
+surf(X', Y', cT)
+c = colorbar;
+c.Label.String = 'cT';
+
+for i = 1:size(data.y_grid, 1)
+    [~, locs] = max(data.cT(i, :));
+    plot3(data.x_grid(i,locs), data.y_grid(i,locs), data.cT(i, locs), 'r.', 'Markersize', 15)
+end
+
+title('cT(AWA, $\delta_s$)', 'Interpreter', 'Latex')
+xlabel('AWA [deg]', 'Interpreter', 'Latex');
+ylabel('sheeting angle $\delta_s$ [deg]', 'Interpreter', 'Latex');
+savefig(strcat(dir,'tacking\cT_SA_AWA.fig'))
+hold off;
+
+% 100 AWA
+sheeting_angle = linspace(deg2rad(-110), deg2rad(-60), 50);
+yaw = linspace(deg2rad(80), deg2rad(125), 45); 
+cT = zeros(length(yaw), length(sheeting_angle));
+
+tic
+for i = 1: length(yaw)
+    ship.yaw = yaw(i);
+    for j = 1:length(sheeting_angle)
+        [~,~,cT(i, j),~] = calc_objective(sheeting_angle(j));
+    end
+end
+toc
+
+figure(11); clf(11); hold on;
+[X,Y] = meshgrid(rad2deg(yaw), rad2deg(sheeting_angle));
+
+% Uncomment lines below to save struct
+data.cT = cT;
+data.x_grid = X';
+data.y_grid = Y';
+save(strcat(dir,'AWA_100\cT_SA_AWA.mat'), 'data');
+
+surf(X', Y', cT)
+c = colorbar;
+c.Label.String = 'cT';
+
+for i = 1:size(data.y_grid, 1)
+    [~, locs] = max(data.cT(i, :));
+    plot3(data.x_grid(i,locs), data.y_grid(i,locs), data.cT(i, locs), 'r.', 'Markersize', 15)
+end
+
+title('cT(AWA, $\delta_s$)', 'Interpreter', 'Latex')
+xlabel('AWA [deg]', 'Interpreter', 'Latex');
+ylabel('sheeting angle $\delta_s$ [deg]', 'Interpreter', 'Latex');
+savefig(strcat(dir,'AWA_100\cT_SA_AWA.fig'))
+hold off;
