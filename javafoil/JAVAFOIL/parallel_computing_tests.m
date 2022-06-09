fe878373e19c6ca5f8780cac64a524fa29f8b9ec
Alexandre Rocha <avrocha99@gmail.com>
2022-06-09 10:04:08 +0200

    parallel_computing_tests: Test parfeval

--- javafoil/JAVAFOIL/parallel_computing_tests.m
+++ javafoil/JAVAFOIL/parallel_computing_tests.m
@@ -23,37 +23,36 @@ fun = @(delta_1, delta_2, localShip, task_id) getfield(calc_objective_mod([delta
                                                 localShip, task_id), 'cT');
 fun2 = @(delta_1, delta_2) delta_1 * delta_2;
 
-sangle_1 = deg2rad(linspace(1, 10, 10));
-sangle_2 = deg2rad(linspace(11, 20, 10));
-Ldelta_1 = length(sangle_1);
-Ldelta_2 = length(sangle_2);
+sheeting_angle_1 = deg2rad(linspace(1, 10, 10));
+sheeting_angle_2 = deg2rad(linspace(11, 20, 10));
+L1 = length(sheeting_angle_1);
+L2 = length(sheeting_angle_2);
 
-a = zeros(Ldelta_1, Ldelta_2);
-b = zeros(Ldelta_1, Ldelta_2);
+a = zeros(L1, L2);
+b = zeros(L1, L2);
+a_par = zeros(L1, L2);
+b_par = zeros(L1, L2);
 
-a_par = zeros(Ldelta_1, Ldelta_2);
-b_par = zeros(Ldelta_1, Ldelta_2);
-
-%% Serial
+%% PARFOR tests
+% Serial
 tic
-for i = 1:Ldelta_1
-    for j = 1:Ldelta_2
-        a(i,j) = fun(sangle_1(i), sangle_2(j), localShip, 1);
-        b(i,j) = fun2(sangle_1(i), sangle_2(j));
+for i = 1:L1
+    for j = 1:L2
+        a(i,j) = fun(sheeting_angle_1(i), sheeting_angle_2(j), localShip, 1);
+        b(i,j) = fun2(sheeting_angle_1(i), sheeting_angle_2(j));
     end
 end
 toc
 
-%% Parallel
+% Parallel
 parpool('local', 4);
 
 tic
 % ticBytes(gcp);
-parfor i = 1:Ldelta_1
-    d1 = sangle_1(i);
-    for j = 1:Ldelta_2
-        a_par(i,j) = fun(d1, sangle_2(j), localShip, getCurrentTask().ID);
-        b_par(i,j) = fun2(d1, sangle_2(j));
+parfor i = 1:L1
+    for j = 1:L2
+        a_par(i,j) = fun(sheeting_angle_1(i), sheeting_angle_2(j), localShip, getCurrentTask().ID);
+        b_par(i,j) = fun2(sheeting_angle_1(i), sheeting_angle_2(j));
     end
 end
 % tocBytes(gcp)
@@ -62,3 +61,37 @@ toc
 poolobj = gcp('nocreate');
 delete(poolobj);
 
+
+%% PARFEVAL tests
+p = parpool('local', 2);
+
+% Background computation
+esc_loop = @(sheeting_angle_1, sheeting_angle_2, ship, task_id) gb_loop(fun, sheeting_angle_1, sheeting_angle_2, ship, task_id);
+F = parfeval(esc_loop, 1, sheeting_angle_1, sheeting_angle_2, localShip, 2);
+
+% Main worker loop
+cT = zeros(L1, L2);
+for i = 1:L1
+    for j = 1:L2
+        cT(i,j) = fun(sheeting_angle_1(i), sheeting_angle_2(j), localShip, 1);
+    end
+end
+
+cT_background = fetchOutputs(F);
+
+poolobj = gcp('nocreate');
+delete(poolobj);
+
+%% Functions
+% GB-ESC loop
+function cT = gb_loop(f, sheeting_angle_1, sheeting_angle_2, ship, task_id)
+    L1 = length(sheeting_angle_1);
+    L2 = length(sheeting_angle_2);
+    cT = zeros(L1, L2);
+
+    for i = 1:L1
+        for j = 1:L2
+            cT(i,j) = f(sheeting_angle_1(i), sheeting_angle_2(j), ship, task_id);
+        end
+    end
+end
