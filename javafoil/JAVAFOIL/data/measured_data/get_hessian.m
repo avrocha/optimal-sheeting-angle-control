e435057e17d9318e338d13120103edc426a61f6c
Alexandre Rocha <avrocha99@gmail.com>
2022-06-26 21:52:12 +0200

    get_hessian: Draft of 4D Hessian implementation

--- javafoil/JAVAFOIL/data/measured_data/get_hessian.m
+++ javafoil/JAVAFOIL/data/measured_data/get_hessian.m
@@ -49,16 +49,16 @@ hess = [gxx(2, 2), gxy(2, 2);
 
 inv(hess)
 
-%% 4D - Get Interpolated Hessian
+%% 4D - Get Interpolated Hessian (USE SHIP STRUCTURES - [WIP])
 clear
-
+addpath lib
 % Load struct
 load('data\measured_data\awa_100\cT_4D.mat');
 % load('data\measured_data\awa_pm_45\cT_4D.mat');
 
 % Operating point
 AWA           = deg2rad(90);
-sheet_angle_0 = [deg2rad(-65.7094), deg2rad(-68.8178), deg2rad(-71.3521), deg2rad(-74.8501)];
+sheet_angle_0 = [deg2rad(-65.7094), deg2rad(-68.8178), deg2rad(-71.3521), deg2rad(-74.8501)]';
 
 % Get AWA neighbors
 
@@ -68,8 +68,30 @@ sheet_angle_0 = [deg2rad(-65.7094), deg2rad(-68.8178), deg2rad(-71.3521), deg2ra
 % if cT neighborhood is < size(3,3) -> error
 % get hessian matrix for each central element
 
+% Get sheeting angle spacing
+dsa           = data.sheeting_angle(1, 2, 1) - data.sheeting_angle(1, 1, 1);
+sa_axes       = zeros(4, 3);
+sa_axes(1, :) = sheet_angle_0(1)-dsa:dsa:sheet_angle_0(1)+dsa;
+sa_axes(2, :) = sheet_angle_0(2)-dsa:dsa:sheet_angle_0(2)+dsa;
+sa_axes(3, :) = sheet_angle_0(3)-dsa:dsa:sheet_angle_0(3)+dsa;
+sa_axes(4, :) = sheet_angle_0(4)-dsa:dsa:sheet_angle_0(4)+dsa;
 
-% Interpolate each hessian element in AWA
+% Get cT neighborhood
+cT_neigh = zeros(length(sa_axes(1, :)), length(sa_axes(3, :)), length(sa_axes(3, :)), length(sa_axes(4, :)));
+
+%% [WIP]
+% Criterion
+J = @(sheeting_angle, ship)(getfield(calc_objective_mod(sheeting_angle, ship, 2), 'cT'));    
+    
+for i = 1:length(sa_axes(1, :))
+    for j = length(sa_axes(2, :))
+        for k = length(sa_axes(3, :))
+            for l = length(sa_axes(4, :))
+                cT_neigh(i, j, k, l) = interp_criterion_irregular();            
+            end
+        end
+    end
+end
 
 % [WIP]
 % Local (numerical) hessian
